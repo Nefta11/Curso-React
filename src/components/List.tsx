61094257771c470ac67a3feae10be3d3126df4cf
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-14 22:17:02 -0600

    feat: añade manejador de clics a los elementos de la lista en el componente List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,10 +3,17 @@ type Props = {
 }
 
 function List({ data }: Props) {
+
+    const handleClick = () => {
+        () => console.log('click');
+    };
+
     return (
         <ul className="list-group">
             {data.map((Elemento) => (
-                <li key={Elemento} className="list-group-item">{Elemento}</li>
+                <li onClick={handleClick} key={Elemento} className="list-group-item">
+                    {Elemento}
+                </li>
             ))}
         </ul>)
 }
