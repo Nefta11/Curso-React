a22fa3623bc0a1d63fe3f4756ce6476ef61e51fc
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-23 21:46:41 -0600

    refactor: mejora la legibilidad del código en el componente List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,18 +5,18 @@ type Props = {
 }
 
 function List({ data }: Props) {
-   const [index, setIndex] =useState(1);
-    const handleClick = (i : number) => {
-        setIndex (i);
+    const [index, setIndex] = useState(1);
+    const handleClick = (i: number) => {
+        setIndex(i);
     };
 
     return (
         <ul className="list-group">
             {data.map((Elemento, i) => (
-                <li 
-                onClick={() => handleClick(i)} 
-                key={i} 
-                className={`list-group-item ${index == i ? 'active' : ''}`}>
+                <li
+                    onClick={() => handleClick(i)}
+                    key={i}
+                    className={`list-group-item ${index == i ? 'active' : ''}`}>
                     {Elemento}
                 </li>
             ))}
